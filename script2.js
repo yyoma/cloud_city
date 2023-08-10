ca2d6661d383c79a5ff1a87852856dfc485d80b5
yyoma <1000000percenttime@gmail.com>
2023-08-10 12:19:13 -0500

    ok
    Co-authored-by: Troy999s <Troy999s@users.noreply.github.com>

--- script2.js
+++ script2.js
@@ -8,7 +8,7 @@ let randint = Math.floor(Math.random() *  4)
 
 recom.src = recomended[randint]
 
-afterbegin
+
 if (randint == 1){
     rec.insertAdjacentText("afterbegin","Experience a little bit of Japan with this shirt with beautiful cherry blossoms featured.");
 }
@@ -16,7 +16,7 @@ else if(randint == 2){
     rec.insertAdjacentHTML("afterbegin" ,
     "100% cotton, featuring a sleek black-bordered square with a dynamic wave and red circle. Bold Cloud City below.");
 }else if(randint == 3){
-    rec.insertAdjacentHTML("","Cloud City's first produced shirt. Support the legacy with this shirt, featuring a minmalistic red flower.");
+    rec.insertAdjacentHTML("afterbegin","Cloud City's first produced shirt. Support the legacy with this shirt, featuring a minmalistic red flower.");
 }else{
     rec.insertAdjacentHTML("afterbegin","Rose 1, a black shirt utlizing 2 roses on the front and back. Best Selling.");
 }
