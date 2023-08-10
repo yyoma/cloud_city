87ce9c2aa6f82de375c3be4facdfac16ab39368b
yyoma <1000000percenttime@gmail.com>
2023-08-10 10:56:41 -0500

    new

--- script2.js
+++ script2.js
@@ -1,9 +1,26 @@
+let rec = document.getElementById('des')
 let recom = document.getElementById('rand');
 
-let recomended = ['rose 2.PNG', 'saku.PNG', 'cloud.PNG', 'flower.PNG']
+let recomended = ['rose 2.PNG', 'saku.PNG', 'cloud.PNG', 'flower1.PNG']
 
-let randint = math.floor(random(1,4) *  10)
+let randint = Math.floor(Math.random() *  4)
 
+
+recom.src = recomended[randint]
+
+afterbegin
 if (randint == 1){
-    
+    rec.insertAdjacentText("afterbegin","Experience a little bit of Japan with this shirt with beautiful cherry blossoms featured.");
 }
+else if(randint == 2){
+    rec.insertAdjacentHTML("afterbegin" ,
+    "100% cotton, featuring a sleek black-bordered square with a dynamic wave and red circle. Bold Cloud City below.");
+}else if(randint == 3){
+    rec.insertAdjacentHTML("","Cloud City's first produced shirt. Support the legacy with this shirt, featuring a minmalistic red flower.");
+}else{
+    rec.insertAdjacentHTML("afterbegin","Rose 1, a black shirt utlizing 2 roses on the front and back. Best Selling.");
+}
+    
+
+
+
